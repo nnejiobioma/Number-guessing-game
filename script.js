cac93d9a90532ff18a31bfe62ebcaedf49648558
nnejiobioma <nnejiobioma@gmail.com>
2022-10-10 13:21:54 +0100

    Third commit"

--- script.js
+++ script.js
@@ -2,36 +2,36 @@ var msg1 = document.getElementById("message1");
 var msg2 = document.getElementById("message2");
 var msg3 = document.getElementById("message3");
 
-var answer = Math.floor(Math.random()*100) + 1;
+var answer = Math.floor(Math.random()*2) + 1;
 var no_of_guesses = 0;
 var guessed_nums = [];
 
 function play(){
     var user_guess = document.getElementById("guess").value;
-    if(user_guess < 1 || user_guess > 100){
-        alert("Please enter a number between 1 and 100.");
+    if(user_guess < 0.5 || user_guess > 2){
+        alert("Please enter a number between 0.5 and 2.");
     }
     else{
         guessed_nums.push(user_guess);
-        no_of_guesses+= 1;
+        no_of_guesses+= 0.5;
 
         if(user_guess < answer){
-            msg1.textContent = "Your guess is too low.";
+            msg1.textContent = "Oh! 🤔 your guess is too low.";
             msg2.textContent = "No. of guesses: " + no_of_guesses;
             msg3.textContent = "Guessed numbers are: " +
             guessed_nums;
         }
         else if(user_guess > answer){
-            msg1.textContent = "Your guess is too high.";
+            msg1.textContent = "Oh!🤔 your guess is too high.";
             msg2.textContent = "No. of guesses: " + no_of_guesses;
             msg3.textContent = "Guessed numbers are: " +
             guessed_nums;
         }
         else if(user_guess == answer){
-            msg1.textContent = "Yippie You Win!!";
+            msg1.textContent = "Hurray!!!! You Win!!";
             msg2.textContent = "The number was: " + answer;
-            msg3.textContent = "You guessed it in "+ no_of_guesses + " guesses";
-            document.getElementById("my_btn").disabled = true;
+            msg3.textContent = "You guessed it in "+ no_of_guesses + " guesses. Please move to next stage";
+            // document.getElementById("my_btn").disabled = true;
         }
     }
 }
